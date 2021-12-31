08d5c5d100aff0e1602f7d36a9450d8d3314c977
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-31 12:18:25 +0600

    added who is

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-console.log('This example is different!');
-console.log('The result is displayed in the Command Line Interface');
+var whois = require('node-whois');
+
+whois.lookup('103.124.251.38', function(err, data) {
+  console.log(err, data);
+});
